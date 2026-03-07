a34cc72427c1688eb24ccef66a01e0ea3ecded61
lucasminecraft9999-rgb <lucasminecraft9999@gmail.com>
2026-03-07 16:14:27 -0300

    comecando o projeto

---
+++ src/main/java/com/ProjetoExtensao/CoinEdu/CoinEduApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetoExtensao.CoinEdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinEduApplication.class, args);
+	}
+
+}
